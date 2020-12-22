23e7c51cf815bb635e7d1be91317e19695f5a853
vishalbidarkar <vishalmercer@msitprogram.net>
2020-12-23 01:35:47 +0530

    commit

--- cgl/src/cgl/CGLBoard.java
+++ cgl/src/cgl/CGLBoard.java
@@ -3,6 +3,7 @@ package cgl;
 public class CGLBoard {
 	public static void main(String Args[]) {
 		System.out.println("Hello GIT2!");
+		System.out.println("Hello GIT3!");
 	}
 
 }
--- cgl/src/cgl/ConwaysGameOfLife.java
+++ cgl/src/cgl/ConwaysGameOfLife.java
@@ -3,6 +3,7 @@ package cgl;
 public class ConwaysGameOfLife {
 	public static void main(String Args[]) {
 		System.out.println("Hello GIT!");
+		System.out.println("Hello GIT4!");
 	}
 
 }
