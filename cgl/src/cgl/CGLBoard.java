05f327a13fb061c4200efba2eb2a3fb875019c8d
vishalbidarkar <vishalmercer@msitprogram.net>
2020-12-23 01:33:53 +0530

    initial commit

---
+++ cgl/src/cgl/CGLBoard.java
@@ -0,0 +1,8 @@
+package cgl;
+
+public class CGLBoard {
+	public static void main(String Args[]) {
+		System.out.println("Hello GIT2!");
+	}
+
+}
---
+++ cgl/src/cgl/ConwaysGameOfLife.java
@@ -0,0 +1,8 @@
+package cgl;
+
+public class ConwaysGameOfLife {
+	public static void main(String Args[]) {
+		System.out.println("Hello GIT!");
+	}
+
+}
